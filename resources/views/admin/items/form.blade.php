57b0ed8e80a19991fa604b3b736e8a699341328e
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2020-03-19 01:13:02 +0530

    Add extra form controlers

--- resources/views/admin/items/form.blade.php
+++ resources/views/admin/items/form.blade.php
@@ -39,43 +39,64 @@
 
 <div class="form-group {{ $errors->has('dolour_rate') ? 'has-error' : ''}}">
     <label for="dolour_rate" class="control-label">{{ 'Dolour Rate' }}</label>
-    <input class="form-control" name="dolour_rate" type="number" id="dolour_rate" value="{{ isset($item->dolour_rate) ? $item->dolour_rate : ''}}" >
+    <div class="input-group-prepend">
+        <span class="input-group-text">Rs</span>
+        <input class="form-control" name="dolour_rate" type="number" id="dolour_rate" value="{{ isset($item->dolour_rate) ? $item->dolour_rate : ''}}" >
+    </div>
     {!! $errors->first('dolour_rate', '<p class="help-block">:message</p>') !!}
 </div>
 
 <div class="form-group {{ $errors->has('product_cost') ? 'has-error' : ''}}">
     <label for="product_cost" class="control-label">{{ 'Product Cost' }}</label>
-    <input class="form-control" name="product_cost" type="number" id="product_cost" value="{{ isset($item->product_cost) ? $item->product_cost : ''}}" >
+    <div class="input-group-prepend">
+        <span class="input-group-text">$</span>
+        <input class="form-control" name="product_cost" type="number" id="product_cost" value="{{ isset($item->product_cost) ? $item->product_cost : ''}}" >
+    </div>
     {!! $errors->first('product_cost', '<p class="help-block">:message</p>') !!}
 </div>
 
 <div class="form-group {{ $errors->has('discount') ? 'has-error' : ''}}">
     <label for="discount" class="control-label">{{ 'Discount' }}</label>
-    <input class="form-control" name="discount" type="number" id="discount" value="{{ isset($item->discount) ? $item->discount : ''}}" >
+    <div class="input-group-prepend">
+        <span class="input-group-text">$</span>
+        <input class="form-control" name="discount" type="number" id="discount" value="{{ isset($item->discount) ? $item->discount : ''}}" >
+    </div>
     {!! $errors->first('discount', '<p class="help-block">:message</p>') !!}
 </div>
 
 <div class="form-group {{ $errors->has('tax') ? 'has-error' : ''}}">
     <label for="tax" class="control-label">{{ 'Tax' }}</label>
-    <input class="form-control" name="tax" type="number" id="tax" value="{{ isset($item->tax) ? $item->tax : ''}}" >
+    <div class="input-group-prepend">
+        <span class="input-group-text">$</span>
+        <input class="form-control" name="tax" type="number" id="tax" value="{{ isset($item->tax) ? $item->tax : ''}}" >
+    </div>
     {!! $errors->first('tax', '<p class="help-block">:message</p>') !!}
 </div>
 
 <div class="form-group {{ $errors->has('clearance_charge') ? 'has-error' : ''}}">
     <label for="clearance_charge" class="control-label">{{ 'Clearance Charge' }}</label>
-    <input class="form-control" name="clearance_charge" type="number" id="clearance_charge" value="{{ isset($item->clearance_charge) ? $item->clearance_charge : ''}}" >
+    <div class="input-group-prepend">
+        <span class="input-group-text">Rs</span>
+        <input class="form-control" name="clearance_charge" type="number" id="clearance_charge" value="{{ isset($item->clearance_charge) ? $item->clearance_charge : ''}}" >
+    </div>
     {!! $errors->first('clearance_charge', '<p class="help-block">:message</p>') !!}
 </div>
 
 <div class="form-group {{ $errors->has('slmarket_price') ? 'has-error' : ''}}">
     <label for="slmarket_price" class="control-label">{{ 'Slmarket Price' }}</label>
-    <input class="form-control" name="slmarket_price" type="number" id="slmarket_price" value="{{ isset($item->slmarket_price) ? $item->slmarket_price : ''}}" >
+    <div class="input-group-prepend">
+        <span class="input-group-text">Rs</span>
+        <input class="form-control" name="slmarket_price" type="number" id="slmarket_price" value="{{ isset($item->slmarket_price) ? $item->slmarket_price : ''}}" >
+    </div>
     {!! $errors->first('slmarket_price', '<p class="help-block">:message</p>') !!}
 </div>
 
 <div class="form-group {{ $errors->has('selling_price') ? 'has-error' : ''}}">
     <label for="selling_price" class="control-label">{{ 'Selling Price' }}</label>
-    <input class="form-control" name="selling_price" type="number" id="selling_price" value="{{ isset($item->selling_price) ? $item->selling_price : ''}}" >
+    <div class="input-group-prepend">
+        <span class="input-group-text">Rs</span>
+        <input class="form-control" name="selling_price" type="number" id="selling_price" value="{{ isset($item->selling_price) ? $item->selling_price : ''}}" >
+    </div>
     {!! $errors->first('selling_price', '<p class="help-block">:message</p>') !!}
 </div>
 
