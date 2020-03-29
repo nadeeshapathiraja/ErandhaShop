d16edf686d86b4665abe4876dd0a174030d73266
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2020-03-29 14:46:46 +0530

    add payment type

--- resources/views/admin/paymenttypes/form.blade.php
+++ resources/views/admin/paymenttypes/form.blade.php
@@ -4,14 +4,24 @@
     {!! $errors->first('order_id', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
-    <label for="name" class="control-label">{{ 'Name' }}</label>
+    <label for="name" class="control-label">{{ 'Customer Name' }}</label>
     <input class="form-control" name="name" type="text" id="name" value="{{ isset($paymenttype->name) ? $paymenttype->name : ''}}" >
     {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('deposit_type') ? 'has-error' : ''}}">
-    <label for="deposit_type" class="control-label">{{ 'Payment Type' }}</label>
-    <input class="form-control" name="deposit_type" type="text" id="deposit_type" value="{{ isset($paymenttype->deposit_type) ? $paymenttype->deposit_type : ''}}" >
+    <label for="deposit_type" class="control-label">{{ 'Payment  Type' }}</label>
+    <select name="deposit_type" id="deposit_type" value="{{ isset($paymenttype->deposit_type) ? $paymenttype->deposit_type : ''}}">
+        <option value="Bank Deposit">Bank Deposit</option>
+        <option value="Cash On Delivery">Cash On Delivery</option>
+        <option value="Pick Up">Pick Up</option>
+        <option value="Pre Order">Pre Order</option>
+    </select>
     {!! $errors->first('deposit_type', '<p class="help-block">:message</p>') !!}
+</div>
+<div class="form-group {{ $errors->has('amount') ? 'has-error' : ''}}">
+    <label for="amount" class="control-label">{{ 'Amount' }}</label>
+    <input class="form-control" name="amount" type="number" id="amount" value="{{ isset($paymenttype->amount) ? $paymenttype->amount : ''}}" >
+    {!! $errors->first('amount', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('amount') ? 'has-error' : ''}}">
     <label for="amount" class="control-label">{{ 'Amount' }}</label>
